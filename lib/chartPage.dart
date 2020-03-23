445adb7c14c9dbc3eddf07fa4ba1688152f2b35e
Suyog Ghinmine <suyogghinmine@gmail.com>
2020-03-23 16:03:24 +0530

    line chart added

--- lib/chartPage.dart
+++ lib/chartPage.dart
@@ -2,82 +2,116 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:charts_flutter/flutter.dart' as charts;
 
-class CasesList extends StatefulWidget {
+class ChartsPage extends StatefulWidget {
   @override
-  _CasesListState createState() => _CasesListState();
+  _ChartsPageState createState() => _ChartsPageState();
 }
 
-class _CasesListState extends State<CasesList> {
+class _ChartsPageState extends State<ChartsPage> {
+  List<charts.Series<LineChartData, int>> _lineGraphData;
+
+  Widget drawChart() {
+    List<LineChartData> getConfirmedCases() {
+      List<LineChartData> confirmedlist = [];
+      for (int i = 0; i < countryData.length; i++) {
+        var date = (countryData[i]["date"]);
+        // print(date);
+        var confirmed = countryData[i]["confirmed"];
+
+        var d = new LineChartData(i, confirmed);
+        confirmedlist.add(d);
+      }
+      return confirmedlist;
+    }
+
+    List<LineChartData> getRecoveredCases() {
+      List<LineChartData> recoveredlist = [];
+      for (int i = 0; i < countryData.length; i++) {
+        var date = (countryData[i]["date"]);
+        // print(date);
+        var recovered = countryData[i]["recovered"];
+
+        var d = new LineChartData(i, recovered);
+        recoveredlist.add(d);
+      }
+      return recoveredlist;
+    }
+
+    List<LineChartData> getDeathCases() {
+      List<LineChartData> deathslist = [];
+      for (int i = 0; i < countryData.length; i++) {
+        var date = (countryData[i]["date"]);
+        // print(date);
+        var deaths = countryData[i]["deaths"];
+
+        var d = new LineChartData(i, deaths);
+        deathslist.add(d);
+      }
+      return deathslist;
+    }
+
+    var confirmeddata = getConfirmedCases();
+
+    var recovereddata = getRecoveredCases();
+
+    var deathdata = getDeathCases();
+
+    _lineGraphData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Colors.blue),
+        id: 'Confirmed',
+        data: confirmeddata,
+        domainFn: (LineChartData sales, _) => sales.yearval,
+        measureFn: (LineChartData sales, _) => sales.salesval,
+      ),
+    );
+    _lineGraphData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Colors.green),
+        id: 'Air Pollution',
+        data: recovereddata,
+        domainFn: (LineChartData sales, _) => sales.yearval,
+        measureFn: (LineChartData sales, _) => sales.salesval,
+      ),
+    );
+    _lineGraphData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Colors.red),
+        id: 'Air Pollution',
+        data: deathdata,
+        domainFn: (LineChartData sales, _) => sales.yearval,
+        measureFn: (LineChartData sales, _) => sales.salesval,
+      ),
+    );
+
+    return charts.LineChart(
+      _lineGraphData,
+      animate: true,
+      // barGroupingType: charts.BarGroupingType.grouped,
+      //behaviors: [new charts.SeriesLegend()],
+      animationDuration: Duration(seconds: 2),
+      
+    );
+  }
+
   var entireJsonResponse;
   String countryName;
   var countryData;
   List<String> countryList;
 
+  bool incorrectCountryName = false;
+
   var showResults = false;
   var loadingJson = true;
-
-  Widget _listViewBuilder(BuildContext context, AsyncSnapshot snapshot) {
-    List<dynamic> values = snapshot.data;
-
-    return ListView.builder(
-       
-        shrinkWrap: true,
-        itemCount: values.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            color: (index % 2 == 0) ? Color(0xFFd6d6d6) : Colors.white,
-            child: ListTile(
-                leading: Text((values[index]["date"]).toString()),
-                title: Container(
-                  child: Row(
-                    children: [
-                      Text(
-                        (values[index]["confirmed"]).toString(),
-                        style: TextStyle(color: Colors.blue, fontSize: 25.00),
-                      ),
-                      SizedBox(
-                        width: 1.0,
-                      ),
-                      Text('confirmed'),
-                    ],
-                  ),
-                ),
-                subtitle: Container(
-                  child: Column(
-                    children: <Widget>[
-                      Row(children: [
-                        Text(
-                          (values[index]["recovered"]).toString(),
-                          style:
-                              TextStyle(color: Colors.green, fontSize: 25.00),
-                        ),
-                        SizedBox(
-                          width: 1.0,
-                        ),
-                        Text('recovered'),
-                      ]),
-                      Row(children: [
-                        Text(
-                          (values[index]["deaths"]).toString(),
-                          style: TextStyle(color: Colors.red, fontSize: 25.00),
-                        ),
-                        SizedBox(
-                          width: 1.0,
-                        ),
-                        Text('deaths'),
-                      ])
-                    ],
-                  ),
-                )),
-          );
-        });
-  }
 
   @override
   void initState() {
     super.initState();
     fetchJsonResponse();
+    // _seriessPieData = List<charts.Series<ChartData, String>>();
+    _lineGraphData = List<charts.Series<LineChartData, int>>();
   }
 
   fetchJsonResponse() async {
@@ -94,7 +128,6 @@ class _CasesListState extends State<CasesList> {
     alphabSortedCountries.sort((a, b) => a.toString().compareTo(b.toString()));
     countryList = alphabSortedCountries;
     entireJsonResponse = valueMap;
-    // jsonData = valueMap;
     return valueMap;
   }
 
@@ -105,21 +138,20 @@ class _CasesListState extends State<CasesList> {
 
     if (entireJsonResponse != null) {
       if (country.length > 0) {
-        // var noSpacesCountryName = country.replaceAll(' ', '');
-        // print(noSpacesCountryName);
         countryName = country;
 
-        // print(entireJsonResponse[countryName]);
         if (entireJsonResponse[countryName] != null) {
           countryData = await entireJsonResponse[countryName];
           var l = await returnData();
           setState(() {
             showResults = true;
-            // incorrectCountryName = false;
-            // generateData();
+            incorrectCountryName = false;
           });
           return (l);
         } else {
+          setState(() {
+            incorrectCountryName = true;
+          });
           print('Wrong country name');
         }
       } else {
@@ -153,14 +185,14 @@ class _CasesListState extends State<CasesList> {
             case ConnectionState.done:
               if (snapshot.hasError) return Text('Error: ${snapshot.error}');
               // return Text('Result: ${snapshot.data}');
-              return _listViewBuilder(context, snapshot);
+              return drawChart();
             default:
               return Text('Some error occured');
           }
         });
 
-    return 
-    Center(
+    return Scaffold(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -191,7 +223,23 @@ class _CasesListState extends State<CasesList> {
                 : Expanded(child: futureBuilder)
           ],
         ),
-      );
-    
+      ),
+    );
   }
+}
+
+// class ChartData {
+//   String date;
+//   int confirmed;
+//   // int recovered;
+//   // int deaths;
+
+//   ChartData(this.date, this.confirmed);
+// }
+
+class LineChartData {
+  int yearval;
+  int salesval;
+
+  LineChartData(this.yearval, this.salesval);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
+import 'casesList.dart';
+import 'chartPage.dart';
 // import 'package:http/http.dart';
 // import 'dart:convert';
 
-import 'casesList.dart';
-
-var jsonData;
-
-void main() {
-  runApp(
-    MyApp(),
-  );
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
@@ -18,11 +12,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+  var _children = [CasesList(), ChartsPage()];
+  changeIndex(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: CasesList()),
+      home: Scaffold(
+        body: _children[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: changeIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.date_range),
+              title: Text('By date'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.insert_chart), title: Text('Graphs')),
+          ],
+        ),
+      ),
     );
   }
 }
